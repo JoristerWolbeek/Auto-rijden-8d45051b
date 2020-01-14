1c8bb154b83b6b1b0abd8786ad7c7b3f03de2176
Joris ter Wolbeek <21074092@talnet.nl>
2020-01-14 12:31:32 +0100

    Uitwerking van de exercise

---
+++ rijbewijs.php
@@ -0,0 +1,7 @@
+<?php
+$leeftijd=readline("hoe oud ben je?");
+    if($leeftijd>16.5){
+        echo(" Je mag beginnen met rijlessen!");
+}   else{
+        echo("Helaas, je mag nog niet beginnen met rijlessen");
+}
